4995242137620cf265c1deb171fc2ce17959d2a5
AndreyPorshnev <andrew.porshnev@yandex.ru>
2025-05-30 15:00:35 +0300

    Добавляет метод получения сессии

--- app/db/database.py
+++ app/db/database.py
@@ -5,3 +5,7 @@ from app.config import settings
 
 engine = create_async_engine(settings.db_settings.db_url)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
+
+async def get_async_session():
+    async with async_session_maker() as session:
+        yield session
